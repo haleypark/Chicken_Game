a3b305d4904338e4c930a6f818819cdd2e7b1f89
Unknown <10706843@my.uvu.edu>
2017-10-23 18:36:58 -0600

    chickenfollow

---
+++ Chicken Game/Assets/scripts/ChickenFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenFollow : MonoBehaviour {
+
+	public Transform target;
+
+	public Transform myTransform;
+
+	void Start () {
+
+		target = gameobject.findwithtag("Player").transform;
+		
+	}
+	
+
+	void Update () {
+		transform.Lookat (target);
+		transform.Translate (Vector3.forward * Time.deltaTime);	
+	}
+}
